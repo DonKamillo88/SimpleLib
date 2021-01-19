e79619c0f46c9a68474154a297ffabba33212a94
DonKamillo <kamil.poplawski88@gmail.com>
2021-01-19 12:25:33 +0000

    x

--- app/src/main/java/org/k2apps/simplelibrary/MainActivity.kt
+++ app/src/main/java/org/k2apps/simplelibrary/MainActivity.kt
@@ -2,10 +2,14 @@ package org.k2apps.simplelibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val a = findViewById<Button>(R.id.button)
+        a.setOnClickListener { org.k2apps.mylibrary.CatsManager.s(this,"dddd") }
     }
 }
--- mylibrary/src/main/java/org/k2apps/mylibrary/CatsManager.kt
+++ mylibrary/src/main/java/org/k2apps/mylibrary/CatsManager.kt
@@ -1,11 +1,15 @@
 package org.k2apps.mylibrary
 
 import android.content.Context
+import android.util.Log
 import android.widget.Toast
 
 
 object CatsManager {
-    fun s(c: Context?, message: String?) {
+    fun s(c: Context, message: String): String {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+        Log.e("TAG", "s: $message")
+
+        return message
     }
 }
